34b373c54872f6dd4afd9eaa3bd472e6b89ff202
Andrew Page <andrew@andrewpage.me>
2015-01-07 18:41:13 -0800

    bash-it added

---
+++ bash-it/custom.bash
@@ -0,0 +1,19 @@
+ssh-add -l ~/.ssh/personal.key
+
+alias be='bundle exec'
+alias gst='git status'
+
+export ANDROID_HOME="/Applications/android-sdk-macosx"
+export GEMDEV=1
+
+function uat {
+    heroku $@ --app shipio-uat
+}
+
+function staging {
+    heroku $@ --app shipio-staging
+}
+
+function production {
+    heroku $@ --app shipio-production
+}
