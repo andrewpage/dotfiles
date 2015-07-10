0f0ae0174ea5f40e67f535507b173b2f1f66c108
Andrew Page <andrew@andrewpage.me>
2015-07-10 15:45:03 -0700

    changes

--- .vimrc
+++ .vimrc
@@ -1,6 +1,5 @@
 filetype plugin indent on
 syntax on
-colorscheme solarized
 
 set background=dark
 set number
--- bash-it/custom.bash
+++ bash-it/custom.bash
@@ -6,6 +6,11 @@ alias berp='be resque-pool'
 export ANDROID_HOME="/Applications/android-sdk-macosx"
 export GEMDEV=1
 
+export INTERCOM_APP_ID='ju23vvqz'
+export INTERCOM_API_KEY='92742b0690b4398c6b338df8c9a7ed9403094594'
+
+alias cds='cd ~/Developer/Ship'
+
 function playground {
     heroku $@ --app shipio-playground
 }
@@ -20,4 +25,16 @@ function staging {
 
 function production {
     heroku $@ --app shipio-production
+}
+
+function scpt {
+    origin=$1
+    origin_filename=$(basename $origin)
+
+    jumphost=$2
+    destination=$3
+
+    echo "SCPing '$origin' through jumphost '$jumphost' to destination '$destination'."
+
+    cat < $origin | ssh $jumphost "cat > $origin_filename && scp $origin_filename $destination && rm $origin_filename"
 }
