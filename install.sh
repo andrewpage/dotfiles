7c99a0172532d2d060e1585fb148ba15afda43fe
Andrew Page <andrew@andrewpage.me>
2016-03-04 00:08:47 -0800

    Beginnings of install script

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+echo "Initializing Andrew's Install Script..."
+echo "---------------------------------------"
+
+# Create Directories
+export DEVELOPER="$HOME/Developer"
+export DOTFILES="$DEVELOPER/dotfiles"
+
+mkdir -p $DEVELOPER
+
+# Symlink Dotfiles
+echo "- Installing dotfiles..."
+for f in bash_profile bashrc gitconfig vimrc
+do
+  echo -e "\t- .$f"
+  ln -s $DOTFILES/$f $HOME/.$f
+done
+
+
