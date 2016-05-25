7df7bbc29c67130019f85511d119237b81f6a296
Andrew Page <andrew@andrewpage.me>
2016-05-25 01:18:04 -0700

    remove alias that i dont use

--- aliases.sh
+++ aliases.sh
@@ -3,6 +3,3 @@ alias dev="cd $HOME/Developer"
 # SSH with my shared personal SSH configuration
 alias pssh="ssh -F $DOTFILES/sshconfig"
 alias pdotfiles="cd $DEVELOPER/dotfiles"
-
-# tmux shortcuts
-alias tmuxinit="ruby $DOTFILES/scripts/init-tmux.rb"
